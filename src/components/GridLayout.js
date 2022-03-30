f5a12ebec9068dbabe12cac531a3a6cddf5e8427
stylboss <63620079+stylboss@users.noreply.github.com>
2022-03-30 12:37:59 +0200

    Layout responsive OK

--- src/components/GridLayout.js
+++ src/components/GridLayout.js
@@ -8,20 +8,7 @@ const GridLayout = () => {
   return (
     <React.Fragment>
       <Banner />
-      <div className="wrapper">
-        <article>
-          <Row
-            title="programme original Netflix"
-            fetchUrl={Requests.fetchNetflixOriginals}
-            isPoster= {true}
-          />
-          <Row title="tendance actuelle" fetchUrl={Requests?.fetchTrending} />
-          <Row title="les mieux notés" fetchUrl={Requests?.fetchTopRated} />
-          <Row title="film d'action" fetchUrl={Requests?.fetchActionMovies} />
-          <Row title="film d'horreur" fetchUrl={Requests?.fetchHorrorMovies} />
-          <Row title="comedie" fetchUrl={Requests?.fetchComedyMovies} isPoster= {true}/>
-          <Row title="documentaire" fetchUrl={Requests?.fetchDocumentaries} />
-        </article>
+      <div className="flexbox">
         <aside>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aperiam
           numquam nesciunt deleniti, delectus nam sed corporis ipsam deserunt
@@ -36,20 +23,37 @@ const GridLayout = () => {
           fugiat omnis qui doloremque maiores aliquid totam, amet sapiente
           temporibus eos facilis.
         </aside>
-        <footer>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          similique laboriosam quas. Itaque dolorum nobis tenetur dolorem
-          quidem, dolore distinctio harum, quia maiores ut quae molestias cum
-          ducimus facere aliquid tempora inventore quisquam commodi nulla?
-          Nostrum suscipit porro quo error eos aliquam molestias perferendis
-          repellendus illo neque ut ad, tenetur rerum voluptatem optio possimus
-          laboriosam ducimus quasi voluptatibus minima reiciendis omnis! Ipsum
-          eos quam tempora repellat, maiores sequi id! A nulla ipsam quaerat
-          deserunt autem veniam tempora, voluptatem voluptatum ipsa? Magnam
-          cupiditate corrupti, ea est voluptatibus rem dolore quibusdam, odio
-          non perferendis iure quia pariatur saepe adipisci harum tempora.
-        </footer>
+        <article>
+          <Row
+            title="programme original Netflix"
+            fetchUrl={Requests.fetchNetflixOriginals}
+            isPoster={true}
+          />
+          <Row title="tendance actuelle" fetchUrl={Requests?.fetchTrending} />
+          <Row title="les mieux notés" fetchUrl={Requests?.fetchTopRated} />
+          <Row title="film d'action" fetchUrl={Requests?.fetchActionMovies} />
+          <Row title="film d'horreur" fetchUrl={Requests?.fetchHorrorMovies} />
+          <Row
+            title="comedie"
+            fetchUrl={Requests?.fetchComedyMovies}
+            isPoster={true}
+          />
+          <Row title="documentaire" fetchUrl={Requests?.fetchDocumentaries} />
+        </article>
       </div>
+      <footer>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        similique laboriosam quas. Itaque dolorum nobis tenetur dolorem quidem,
+        dolore distinctio harum, quia maiores ut quae molestias cum ducimus
+        facere aliquid tempora inventore quisquam commodi nulla? Nostrum
+        suscipit porro quo error eos aliquam molestias perferendis repellendus
+        illo neque ut ad, tenetur rerum voluptatem optio possimus laboriosam
+        ducimus quasi voluptatibus minima reiciendis omnis! Ipsum eos quam
+        tempora repellat, maiores sequi id! A nulla ipsam quaerat deserunt autem
+        veniam tempora, voluptatem voluptatum ipsa? Magnam cupiditate corrupti,
+        ea est voluptatibus rem dolore quibusdam, odio non perferendis iure quia
+        pariatur saepe adipisci harum tempora.
+      </footer>
     </React.Fragment>
   )
 }
